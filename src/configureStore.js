b240de84245e71202aebbe86b94e14d42e99b4a1
Geoff-Ford <me@geoffford.co.uk>
2017-05-13 14:41:15 +0100

    Added redux dev tools extension

--- src/configureStore.js
+++ src/configureStore.js
@@ -17,6 +17,7 @@ const configureStore = () => {
 
     return createStore(
         todoApp,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
         applyMiddleware(...middlewares)
     );
 };
